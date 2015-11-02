7f5135f7347a08d7b8badc65164c23db130ccf64
francisco-uttn <francisco.portilla@uttn.mx>
2015-11-02 08:46:18 -0600

    Parte 1.0.1

---
+++ App_Code/Ctrl_Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Ctrl_Login
+/// </summary>
+public class Ctrl_Login
+{
+    public Ctrl_Login()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        int a;
+    }
+}
